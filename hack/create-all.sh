227a0b964e7b30ff14a6b00241f6258308e00e5b
Ravi Trivedi <travi@redhat.com>
2021-04-19 13:19:03 +0530

    Reverting machine scaling for AWS IPI

--- hack/create-all.sh
+++ hack/create-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 oc create -f deploy/namespace.yaml
+oc project openshift-managed-upgrade-operator
 oc create -f deploy/cluster_role.yaml
 oc create -f deploy/cluster_role_binding.yaml
 oc create -f deploy/service_account.yaml
@@ -8,5 +9,6 @@ oc create -f deploy/prometheus_role.yaml
 oc create -f deploy/prometheus_rolebinding.yaml
 oc create -f deploy/managed-upgrade-operator-config.yaml
 oc create -f deploy/crds/upgrade.managed.openshift.io_upgradeconfigs_crd.yaml
-oc create -f deploy/crds/upgrade.managed.openshift.io_v1alpha1_upgradeconfig_cr.yaml
 oc create -f deploy/operator.yaml
+sleep 3
+oc create -f deploy/crds/upgrade.managed.openshift.io_v1alpha1_upgradeconfig_cr.yaml
